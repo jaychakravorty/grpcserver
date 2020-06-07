87b32f7a2d264ffb764551d45821f04e5b74972d
DonCarnage <jayanta.chakravorty@outlook.com>
2020-06-07 14:52:00 +0530

    first commit

---
+++ Services/GreeterService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"{Environment.MachineName} recieved {request.Name}");
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Pong processed at {Environment.MachineName}"
+            });
+        }
+    }
+}
